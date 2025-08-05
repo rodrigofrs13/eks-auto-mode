0c1c7f78d02eb82db206b14bb7a0a1d8ca9d2781
rodrigofrs13 <rodrigofrs13@gmail.com>
2025-08-05 17:08:40 -0300

    Initial setup

---
+++ setup-cluster-eks-auto-mode.sh
@@ -0,0 +1,5 @@
+eksctl create cluster -f ./conf-cluster-eks-auto-mode.yaml
+
+#eksctl upgrade cluster -f ./conf-cluster-eks-auto-mode.yaml
+
+# Loga - 'aws eks --region us-east-1 update-kubeconfig --name cluster-auto-mode'
